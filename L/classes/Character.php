7f0337b5286142c87ee04de35a74db23ddcb36db
Edwin <edwin.barrera1025@gmail.com>
2026-06-16 11:06:40 +0200

    L: deleting extra code not required

--- L/classes/Character.php
+++ L/classes/Character.php
@@ -12,11 +12,3 @@ class Character
         echo "The character moves forward.\n";
     }
 }
-
-class Knight extends Character implements Actions
-{
-    public function attack(): void
-    {
-        echo "The character attacks with a sword.\n";
-    }
-}
